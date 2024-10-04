3cd6a5d16d62619943da6d95f480bc501478d532
ilkou <48165230+ilkou@users.noreply.github.com>
2024-10-04 11:32:11 +0100

    v1

---
+++ README.md
@@ -0,0 +1,4 @@
+# ilkou.github.io
+
+* [react-github-pages](https://github.com/gitname/react-gh-pages)
+* https://www.springboard.com/blog/career-advice/programmer-portfolio/
---
+++ src/portfolio.js
@@ -0,0 +1,337 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: "Oussama Ouklich",
+  title: "Hi all, I'm Oussama",
+  subTitle: emoji(
+      "A passionate Senior Software Engineer 🚀 with experience in building Web, Mobile, and Desktop applications with React Js / Node Js / Spring / React Native / QT and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+      "https://flowcv.com/resume/fnb05mp670",
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+  github: "https://github.com/ilkou",
+  linkedin: "https://www.linkedin.com/in/oussama-ouklich/",
+  gmail: "ilkou.dev@gmail.com",
+  gitlab: "https://gitlab.com/ilkou",
+  // facebook: "https://www.facebook.com/oussama.ouklich/",
+  // Instagram and Twitter are also supported in the links!
+};
+
+// Your Skills Section
+
+const skillsSection = {
+  title: "What i do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+        "⚡ Develop highly interactive Front end / User Interfaces for your web, mobile and desktop applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Integration of third party services such as Firebase"),
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react",
+    },
+    {
+      skillName: "java",
+      fontAwesomeClassname: "fab fa-java",
+    },
+    {
+      skillName: "yarn",
+      fontAwesomeClassname: "fab fa-yarn",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire",
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker",
+    },
+  ],
+};
+
+// Your education background
+
+const educationInfo = {
+  viewEducation: true, // Set it to true to see education section
+  schools: [
+    {
+      schoolName: "National School of Applied Sciences Khouribga",
+      logo: require("./assets/images/ensaLogo.png"),
+      subHeader: "Software Engineer",
+      duration: "September 2016 - July 2021",
+    },
+    {
+      schoolName: "1337 Khouribga (42 Network)",
+      logo: require("./assets/images/1337Logo.png"),
+      subHeader: "Junior Developer",
+      duration: "October 2018 - Present",
+    },
+  ],
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend", //Insert stack or technology you have experience in
+      progressPercentage: "90%", //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "95%",
+    },
+    {
+      Stack: "Desktop Apps",
+      progressPercentage: "65%",
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "95%",
+    },
+  ],
+};
+
+// Your top 3 work experiences
+
+const workExperiences = {
+  viewExperiences: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Senior Software Engineer",
+      company: "Cires Technologies",
+      companylogo: require("./assets/images/cires.jpeg"),
+      date: "August 2022 - Present",
+      desc: "Full Stack Developer Engineer",
+      descBullets: [
+        "Design and develop SaaS solutions for the global logistics gateway TANGER MED"
+      ],
+    },
+      {
+      role: "Software Engineer",
+      company: "QUERIA SERVICES",
+      companylogo: require("./assets/images/logo-queria.jpeg"),
+      date: "February 2021 - July 2022",
+      desc: "Full Stack Developer Spring Boot / React",
+      descBullets: [
+        "Design and develop SaaS solutions using microservices architecture"
+      ],
+    },
+    {
+      role: "Software Developer",
+      company: "ALPHA REAL ESTATE PARTNERS",
+      companylogo: require("./assets/images/arepLogo.jpg"),
+      date: "May 2020 - October 2020",
+      desc:
+          "My first step in a company is an important milestone in the 42 training. This internship entails discovering the professional world and putting work and adaptation skills at the service of the market.",
+      descBullets: [
+        "Project 1: UM6P Virtual Fair",
+        "Project 2: Development of a 3D configurator",
+      ],
+    },
+  ],
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "ilkou", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to false
+};
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/um6pLogo.jpg"),
+      link: "https://www.um6p.ma/fr",
+    },
+    {
+      image: require("./assets/images/tangermed.jpeg"),
+      link: "https://www.tangermed.ma/fr/",
+    },
+  ],
+};
+
+// Your Achievement Section Include Your Certification Talks and More
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+      "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+          "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      footerLink: [
+        {
+          name: "Certification",
+          url:
+              "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing",
+        },
+        {
+          name: "Award Letter",
+          url:
+              "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing",
+        },
+        {
+          name: "Google Code-in Blog",
+          url:
+              "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html",
+        },
+      ],
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+          "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url:
+              "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en",
+        },
+      ],
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        { name: "Certification", url: "" },
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/",
+        },
+      ],
+    },
+  ],
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+      "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url:
+          "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+          "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?",
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+          "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies.",
+    },
+  ],
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+      "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/",
+    },
+  ],
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo",
+  ],
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+      "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: undefined,
+  email_address: "ilkou.dev@gmail.com",
+};
+
+//Twitter Section
+
+const twitterDetails = {
+  userName: "ouklich", //Replace "twitter" with your twitter username without @
+};
+export {
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+};
